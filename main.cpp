76a5af6149096b2e5c1770df114e51e5893a1aac
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-25 13:13:39 +0300

    added isPyth

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
+bool isPyth(unsigned a, unsigned b, unsigned c)
+{
+  bool d = a*a == (b*b + c*c);
+  d = d || (b*b == (a*a + c*c));
+  d = d || (c*c == (a*a + b*b));
+  return d;
+}
+
 int main()
 {
   using u_t = unsigned;
   u_t a = 0;
   size_t count = 0;
   while (std::cin >> a) {
-    count+=a*a;
+    count+=isPyth(a,b,c) ? 1 : 0;
+    c = b;
+    b = a;
   }
-  
+
   if (std::cin.eof()) {
     std::cout << count << "\n";
   } else if (std::cin.fail()) {
