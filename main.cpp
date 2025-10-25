ea838e8eb5bcf4564313d3c1fab72a0dad40bbf6
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-25 15:51:01 +0300

    fix errors

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ unsigned min(unsigned a, unsigned b);
 int main()
 {
   using u_t = unsigned;
-  u_t a = 0;
+  u_t a = 0, b, c;
   size_t count = 0;
   std::cin >> b >> c;
   while (std::cin >> a) {
     try {
       count+=isPyth(a,b,c);
     } catch (const std::overflow_error& e) {
-        std::cerr <<  e.what();
+        std::cerr <<  e.what() << "\n";
         return 2;
     }
     c = b;
@@ -35,9 +35,9 @@ int main()
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
   unsigned biggest = max(a, max(b, c));
-  unsigned smallest = min(a, min(b, c))
+  unsigned smallest = min(a, min(b, c));
   unsigned medium = a + b + c - biggest - smallest;
-  if (isOverMultiply(biggest,biggest) && isOverSum(smallest*smallest, medium*medium)) {
+  if (isOverMultiply(biggest,biggest) || isOverSum(smallest*smallest, medium*medium)) {
     throw std::overflow_error("Can't mulply or sum");
   }
   return biggest*biggest == (medium*medium + smallest*smallest);
