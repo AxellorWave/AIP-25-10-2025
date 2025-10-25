a4a559acaab0a0c97f2ebff0959bd6d647829714
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-25 13:21:59 +0300

    Pyth complete

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 
-bool isPyth(unsigned a, unsigned b, unsigned c)
-{
-  bool d = a*a == (b*b + c*c);
-  d = d || (b*b == (a*a + c*c));
-  d = d || (c*c == (a*a + b*b));
-  return d;
-}
+bool isPyth(unsigned a, unsigned b, unsigned c);
 
 int main()
 {
   using u_t = unsigned;
   u_t a = 0;
   size_t count = 0;
+  std::cin >> b >> c;
   while (std::cin >> a) {
     count+=isPyth(a,b,c) ? 1 : 0;
     c = b;
@@ -22,7 +17,15 @@ int main()
   if (std::cin.eof()) {
     std::cout << count << "\n";
   } else if (std::cin.fail()) {
-    std::cout << "Error\n";
+    std::cout << "Brooo, it is FAIL!!!\n";
     return 1;
   }
+}
+
+bool isPyth(unsigned a, unsigned b, unsigned c)
+{
+  bool d = a*a == (b*b + c*c);
+  d = d || (b*b == (a*a + c*c));
+  d = d || (c*c == (a*a + b*b));
+  return d;
 }
