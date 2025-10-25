e291c4b53d59c9b23eaeddc2afd0628e85948aa0
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-25 13:02:40 +0300

    added an input handler

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+
 int main()
-{}
+{
+  using u_t = unsigned;
+  u_t a = 0;
+  std::cin >> a;
+  if (std::cin.eof()) {
+    std::cout << 0 << "\n";
+  } else if (std::cin.fail()) {
+    std::cout << "Error\n";
+    return 1;
+  }
+}
